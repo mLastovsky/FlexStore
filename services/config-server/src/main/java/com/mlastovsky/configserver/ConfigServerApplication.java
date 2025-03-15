16cf92875fa34f721f68d49f59240b82d2264296
mLastovsky <LastovskyTech@gmail.com>
2025-03-15 21:31:04 +0300

    add: initialize config server

---
+++ services/config-server/src/main/java/com/mlastovsky/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.mlastovsky.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
