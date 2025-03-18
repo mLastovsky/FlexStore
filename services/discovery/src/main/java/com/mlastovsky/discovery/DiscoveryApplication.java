1be383f699fb6a416b52a859ce74e271be884111
mLastovsky <LastovskyTech@gmail.com>
2025-03-18 23:34:50 +0300

    add: initialize discovery service

--- services/discovery/src/main/java/com/mlastovsky/discovery/DiscoveryApplication.java
+++ services/discovery/src/main/java/com/mlastovsky/discovery/DiscoveryApplication.java
@@ -2,14 +2,12 @@ package com.mlastovsky.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class DiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
 
 }
