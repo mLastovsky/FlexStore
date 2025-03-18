3558f95c854469d8c70294cd7cd86001d0a9fc75
mLastovsky <LastovskyTech@gmail.com>
2025-03-19 01:04:31 +0300

    impl: implement Customer Rest controller

---
+++ services/customer/src/main/java/com/mlastovsky/controller/CustomerController.java
@@ -0,0 +1,40 @@
+package com.mlastovsky.controller;
+
+import com.mlastovsky.model.CustomerRequest;
+import com.mlastovsky.model.CustomerResponse;
+import com.mlastovsky.service.CustomerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<String> createCustomer(
+            @RequestBody @Valid CustomerRequest request
+    ) {
+        return ResponseEntity.ok(customerService.createCustomer(request));
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateCustomer(
+            @RequestBody @Valid CustomerRequest request
+    ) {
+        customerService.updateCustomer(request);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponse>> findAll() {
+        return ResponseEntity.ok(customerService.findAllCustomers);
+    }
+
+}
