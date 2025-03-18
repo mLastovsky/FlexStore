4cb90c44520bf4dc3b58adb18d34474853ba9ad6
mLastovsky <LastovskyTech@gmail.com>
2025-03-19 01:11:46 +0300

    feat: added model for represents responses from clients

---
+++ services/customer/src/main/java/com/mlastovsky/model/CustomerResponse.java
@@ -0,0 +1,16 @@
+package com.mlastovsky.model;
+
+public record CustomerResponse(
+
+        String id,
+
+        String firstname,
+
+        String lastname,
+
+        String email,
+
+        Address address
+
+) {
+}
