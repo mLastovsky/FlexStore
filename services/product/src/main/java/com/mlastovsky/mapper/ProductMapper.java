e61482bf5aada4a9f138a95642a689af0cee7d70
mLastovsky <LastovskyTech@gmail.com>
2025-03-19 16:39:17 +0300

    feat: declare ProductMapper toProductResponse method

--- services/product/src/main/java/com/mlastovsky/mapper/ProductMapper.java
+++ services/product/src/main/java/com/mlastovsky/mapper/ProductMapper.java
@@ -3,6 +3,7 @@ package com.mlastovsky.mapper;
 import com.mlastovsky.model.Category;
 import com.mlastovsky.model.Product;
 import com.mlastovsky.model.ProductRequest;
+import com.mlastovsky.model.ProductResponse;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +21,19 @@ public class ProductMapper {
                         .id(request.categoryId())
                         .build())
                 .build();
+    }
+
+    public ProductResponse toProductResponse(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getAvailableQuantity(),
+                product.getPrice(),
+                product.getCategory().getId(),
+                product.getCategory().getName(),
+                product.getCategory().getDescription()
+        );
     }
 
 }
