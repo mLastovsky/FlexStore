234b5ed62c60d1768fe71e40d50a09459fb069c9
mLastovsky <LastovskyTech@gmail.com>
2025-03-19 16:32:26 +0300

    add: custom exception

---
+++ services/product/src/main/java/com/mlastovsky/exception/ProductNotFoundException.java
@@ -0,0 +1,12 @@
+package com.mlastovsky.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ProductNotFoundException extends RuntimeException {
+
+    private final String message;
+
+}
