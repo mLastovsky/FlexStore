04962526d3af5e96e4b1d43d18d860d54fdba6b7
mLastovsky <LastovskyTech@gmail.com>
2025-03-19 16:13:08 +0300

    impl: Product rest controller

---
+++ services/product/src/main/java/com/mlastovsky/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.mlastovsky.controller;
+
+import com.mlastovsky.model.*;
+import com.mlastovsky.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(
+            @RequestBody @Valid ProductRequest request
+    ) {
+        return ResponseEntity.ok(productService.createProduct(request));
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProduct(
+            @RequestBody List<ProductPurchaseRequest> request
+    ) {
+        return ResponseEntity.ok(productService.purchaseProducts(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> findById (
+            @PathVariable Long id
+    ){
+        return ResponseEntity.ok(productService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> findAll (){
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+}
