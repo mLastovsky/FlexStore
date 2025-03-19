a30397ed4b554ff67834eb28d43a05e8102dda26
mLastovsky <LastovskyTech@gmail.com>
2025-03-19 16:50:03 +0300

    feat: added new findByIdIn method

--- services/product/src/main/java/com/mlastovsky/repository/ProductRepository.java
+++ services/product/src/main/java/com/mlastovsky/repository/ProductRepository.java
@@ -2,7 +2,13 @@ package com.mlastovsky.repository;
 
 import com.mlastovsky.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("FROM Product p WHERE p.id IN :ids ORDER BY p.id")
+    List<Product> findAllByIdIn(List<Long> ids);
 
 }
