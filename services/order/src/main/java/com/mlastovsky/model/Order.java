579af50ad8b46c98fe9019953875347fa6aff618
mLastovsky <LastovskyTech@gmail.com>
2025-03-20 15:52:05 +0300

    add: some models

---
+++ services/order/src/main/java/com/mlastovsky/model/Order.java
@@ -0,0 +1,49 @@
+package com.mlastovsky.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "customer_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String reference;
+
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    private String customerId;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderLine> orderLineList;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedDate
+    @Column(insertable = false)
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+
+}
---
+++ services/order/src/main/java/com/mlastovsky/model/OrderLine.java
@@ -0,0 +1,26 @@
+package com.mlastovsky.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="order_id")
+    private Order order;
+
+    private Long productId;
+
+    private Double quantity;
+
+}
