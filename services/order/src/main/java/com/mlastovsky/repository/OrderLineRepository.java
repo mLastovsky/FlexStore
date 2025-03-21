4af9b47804db989bc03cd00360127c14acab1b4c
mLastovsky <LastovskyTech@gmail.com>
2025-03-21 15:19:24 +0300

    feat: add new findAllByOrderId method

--- services/order/src/main/java/com/mlastovsky/repository/OrderLineRepository.java
+++ services/order/src/main/java/com/mlastovsky/repository/OrderLineRepository.java
@@ -3,6 +3,10 @@ package com.mlastovsky.repository;
 import com.mlastovsky.model.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
+
+    List<OrderLine> findAllByOrderId(Long id);
 
 }
