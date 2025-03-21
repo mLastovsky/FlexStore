f3a0c01837e3d828160f47b7cee0b6b6e9af3c68
mLastovsky <LastovskyTech@gmail.com>
2025-03-21 10:38:33 +0300

    add: customerProxy interface

---
+++ services/order/src/main/java/com/mlastovsky/proxy/CustomerProxy.java
@@ -0,0 +1,21 @@
+package com.mlastovsky.proxy;
+
+import com.mlastovsky.model.CustomerResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@FeignClient(
+        name = "customer-service",
+        url = "${application.config.customer-url}"
+)
+@Component
+public interface CustomerProxy {
+
+    @GetMapping("/{id}")
+    Optional<CustomerResponse> findCustomerById(@PathVariable Long id);
+
+}
