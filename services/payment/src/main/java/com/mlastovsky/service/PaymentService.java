3fb4d6e67c0f921b41e5d160887a666737e3e52e
mLastovsky <LastovskyTech@gmail.com>
2025-03-21 16:17:49 +0300

    add: PaymentService

---
+++ services/payment/src/main/java/com/mlastovsky/service/PaymentService.java
@@ -0,0 +1,35 @@
+package com.mlastovsky.service;
+
+import com.mlastovsky.kafka.NotificationProducer;
+import com.mlastovsky.mapper.PaymentMapper;
+import com.mlastovsky.model.PaymentNotificationRequest;
+import com.mlastovsky.model.PaymentRequest;
+import com.mlastovsky.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final PaymentMapper mapper;
+    private final NotificationProducer notificationProducer;
+
+    public Long createPayment(PaymentRequest request) {
+        var payment = paymentRepository.save(mapper.toPayment(request));
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstname(),
+                        request.customer().lastname(),
+                        request.customer().email()
+                )
+        );
+
+        return payment.getId();
+    }
+
+}
