49216033b712cc674e094da6cb07de66e5d1261a
mLastovsky <LastovskyTech@gmail.com>
2025-03-21 12:27:30 +0300

    add: OrderLineRepository interface

---
+++ services/order/src/main/java/com/mlastovsky/repository/OrderLineRepository.java
@@ -0,0 +1,8 @@
+package com.mlastovsky.repository;
+
+import com.mlastovsky.model.OrderLine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
+
+}
